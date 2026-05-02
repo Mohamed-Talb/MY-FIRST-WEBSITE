9866954184295a86fe18fd1e18c4614da2011f82
mtaleb <mtaleb.contact@gmail.com>
2026-05-02 18:41:26 +0100

    Add a serveing of html files

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-import http from "http"
-import path from "path";
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
 
-const PORT = 8000;
+const PORT = 8000
 
-const __dirname = import.meta.dirname;
-console.log(__dirname);
-function ReqResHandler(req, res)
-{
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.end("<html><h1>The server is working</h1></html>");
-}
+const __dirname = import.meta.dirname
 
-const server = http.createServer(ReqResHandler);
-server.listen(PORT, () => {console.log(`http://localhost:${PORT}`)});
+const server = http.createServer(async (req, res) => {
+    await serveStatic(req, res, __dirname)
 
+})
+
+server.listen(PORT, ()=> console.log(`http://localhost:${PORT}`))
